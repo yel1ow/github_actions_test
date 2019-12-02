79122842bb12ed2c85bcb02cd7938773d44ce00e
yel1ow <lv.reg.all@gmail.com>
2019-12-02 09:18:14 +0300

    init

---
+++ Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebApplication1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(5, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.That(5, Is.EqualTo(5));
+        }
+    }
+}
